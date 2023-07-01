d150e7a2b60614e2acc3cea9de947b18b334bd73
ZAKARIAE MOURID <mourid.zakariae@ofppt-edu.ma>
2023-07-01 18:46:34 +0100

    installed dependencies

--- README.md
+++
@@ -1,1 +0,0 @@
-# portfolio-v2
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>React 18 Alpha</h1>
+    </div>
+  )
+}
+export default App
